54f5eab5f5beeed5bc8f0d9652e471b429867919
Ligaya Reyes <l.reyes062018@gmail.com>
2020-07-29 22:21:49 +0800

    created homework files

---
+++ Lesson 7/newObjectExercises.js
@@ -0,0 +1,93 @@
+// Write a JavaScript program to list the properties of a JavaScript object.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+// Write a JavaScript program to delete the rollno property from the following object.
+// Also print the object before or after deleting the property.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// Write a JavaScript program to get the length of a JavaScript object.
+// Sample object :
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+// Write a JavaScript program to display the reading status
+// (i.e. display book name, author name and reading status) of the following books.
+// var library = [ 
+//     {
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Suzanne Collins',
+//         title:  'Mockingjay: The Final Book of The Hunger Games', 
+//         readingStatus: false
+//     }];
+
+
+// Write a JavaScript program to sort an array of JavaScript objects.
+// Sample Object :
+// var library = [ 
+//    {
+//        title:  'The Road Ahead',
+//        author: 'Bill Gates',
+//        libraryID: 1254
+//    },
+//    {
+//        title: 'Walter Isaacson',
+//        author: 'Steve Jobs',
+//        libraryID: 4264
+//    },
+//    {
+//        title: 'Mockingjay: The Final Book of The Hunger Games',
+//        author: 'Suzanne Collins',
+//        libraryID: 3245
+//    }];
+// Expected Output:
+// [[object Object] {
+//   author: "Walter Isaacson",
+//   libraryID: 4264,
+//   title: "Steve Jobs"
+// }, [object Object] {
+//   author: "Suzanne Collins",
+//   libraryID: 3245,
+//   title: "Mockingjay: The Final Book of The Hunger Games"
+// }, [object Object] {
+//   author: "The Road Ahead",
+//   libraryID: 1254,
+//   title: "Bill Gates"
+// }]
+
+
+// Write a JavaScript function to print all the methods in an JavaScript object.
+// Test Data :
+// console.log(all_properties(Array));
+// ["length", "name", "arguments", "caller", "prototype", "isArray", "observe", "unobserve"]
+
+
+// Write a JavaScript function to retrieve all the names of object's own and inherited properties.
+
+
+// Write a JavaScript function to convert an object into a list of `[key, value]` pairs.
+
+
+// Write a JavaScript function to check whether an object contains given property.
